07d1edf350ce5d0f0ec2aea63c63adc0497f94d8
shallowaria <shallowaria@gmail.com>
2025-10-15 22:01:33 +0800

    add:DigitalTwin

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 4. 使用 sphereGeometry 生成一个球体，为其添加地球的材质，用 useFrame 使其自转；同时再根据经纬度计算出其在 3D 图像上的确切位置，生成一个球体代表我的位置，其 mesh 的 position 属性为 lat 和 lng,通过原生 THREEjs 创建一个 CityLabel，用 useFrame 使标签永远面向 camera(即屏幕) ,将其和 position 球打包成一个 gruop，添加 OrbitControls，并在手机上禁用此组件防止用户无法下滑
 5. 将此前的 About 和 Works 页面迁移过来并更新样式,素材来自于[iconfont](https://www.iconfont.cn/)和官网的 icon
 6. 为 Works 添加一个可互动的 3D 电脑屏幕，模型从[Sketchfab](https://sketchfab.com/feed)上下载，并用[此网站](https://gltf.pmnd.rs/)将其转化为可用的代码，然后用 useTexture 添加材质，根据传进来的 currentProject 不同（由项目的左右按钮控制）,在屏幕上呈现不同的图片，折腾我好久：useTexture 默认 flipY 为 true，需要在其内添加返回函数改为 false，并在 mesh 内传入 map 完成修改，然后用 useGSAP 的 from 属性添加触发模型动画，让其看起来更丝滑
+7. Study Route 文字部分:用 group 在父元素控制子元素在 hover 时的状态.3D 部分：在[ReadyPlay](https://readyplayer.me/avatar)网站上建立一个我的 3D 模型，导出为 glb 样式，然后给其转化为 FBX 样式，在[Mixamo](https://www.mixamo.com/)绑定需要的动作后导出为 FBX 样式，将它们存放在 public 内，然后用[此网站](https://gltf.pmnd.rs/)将模型转化成代码并用 useGLTF 导入页面，为其添加自然光，聚光灯，OrbitControls 和 Suspence，然后用 useState 设置其默认动作，使用 onPointerOver 传参，接着加载动画:
+
+- 用 useFBX 加载动画
+- [animationName][0].name 设置动画名称
+- useAnimations 输入动画数组和使用动画的 ref 得到 actions
+- useEffect 内 action.play() 播放动画，同时设置 fadeIn 和 fadeOut 过渡流畅
 
 ### 用到的网站：
 
@@ -17,3 +23,5 @@
 - [maath tutorial](https://sudeeptobose.medium.com/basic-3d-camera-movement-with-react-three-fiber-and-maath-library-4b060bfe7c5c)
 - [Sketchfab](https://sketchfab.com/feed)
 - [GLBF generated](https://gltf.pmnd.rs)
+- [ReadyPlay](https://readyplayer.me/avatar)
+- [Mixamo](https://www.mixamo.com/)
--- src/sections/Study.jsx
+++ src/sections/Study.jsx
@@ -1,11 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import CanvasLoader from "../components/CanvasLoader";
 import DigitalTwin from "../components/DigitalTwin";
 import { studyExperiences } from "../constants/index.js";
 
 const Study = () => {
+  const [animationName, setAnimationName] = useState("idle");
+
   return (
     <section className="sm:px-10 px-5 my-20" id="study">
       <div className="w-full text-zinc-600">
@@ -18,7 +20,11 @@ const Study = () => {
               <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
               <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
               <Suspense fallback={<CanvasLoader />}>
-                <DigitalTwin />
+                <DigitalTwin
+                  animationName={animationName}
+                  position-y={-3}
+                  scale={3}
+                />
               </Suspense>
             </Canvas>
           </div>
@@ -29,6 +35,15 @@ const Study = () => {
                   <div
                     key={id}
                     className="grid grid-cols-[auto_1fr] items-start gap-5  transition-all ease-in-out duration-500 cursor-pointer hover:bg-cyan-200 rounded-lg sm:px-5 px-2.5 group"
+                    onClick={() =>
+                      setAnimationName(animation.toLocaleLowerCase())
+                    }
+                    onPointerOver={() =>
+                      setAnimationName(animation.toLocaleLowerCase())
+                    }
+                    onPointerOut={() => {
+                      setAnimationName("idle");
+                    }}
                   >
                     <div className="flex flex-col h-full justify-start items-center py-2">
                       <div className="rounded-3xl w-16 h-16 p-2 bg-black-600">
